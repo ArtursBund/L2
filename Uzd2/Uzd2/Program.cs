0561a400c450b390ee2ac6c8a971c31d5e1207cf
ArtursBund <bundulis.arturs@gmail.com>
2021-09-07 21:41:43 +0300

    Test 1

---
+++ Uzd2/Uzd2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Uzd2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
